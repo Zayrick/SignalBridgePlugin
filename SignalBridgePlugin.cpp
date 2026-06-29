3f6559e1416374fd327fee5f76500f9319e8f40a
Zayrick <tvefxt@gmail.com>
2026-06-29 20:46:35 +0800

    init

---
+++ SignalBridgePlugin.cpp
@@ -0,0 +1,3 @@
+#include "SignalBridgePlugin.h"
+
+SignalBridgePlugin::SignalBridgePlugin() {}
---
+++ SignalBridgePlugin.h
@@ -0,0 +1,12 @@
+#ifndef SIGNALBRIDGEPLUGIN_H
+#define SIGNALBRIDGEPLUGIN_H
+
+#include "SignalBridgePlugin_global.h"
+
+class SIGNALBRIDGEPLUGIN_EXPORT SignalBridgePlugin
+{
+public:
+    SignalBridgePlugin();
+};
+
+#endif // SIGNALBRIDGEPLUGIN_H
