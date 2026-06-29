293e598f10c41289049ebd22aefffb76e6e8304a
Zayrick <tvefxt@gmail.com>
2026-06-29 22:25:01 +0800

    feat(plugin): implement OpenRGB plugin interface
    Reason:
    - Enable the plugin to expose metadata and lifecycle hooks to OpenRGB.
    
    Changes:
    - Add Qt plugin metadata and OpenRGB interface inheritance.
    - Implement plugin info, API version, load, widget, tray, and unload hooks.
    - Configure version and OpenRGB include paths in CMake.

--- SignalBridgePlugin.cpp
+++ SignalBridgePlugin.cpp
@@ -1,3 +1,55 @@
 #include "SignalBridgePlugin.h"
 
-SignalBridgePlugin::SignalBridgePlugin() {}
+SignalBridgePlugin::SignalBridgePlugin(QObject* parent)
+    : QObject(parent)
+{
+}
+
+SignalBridgePlugin::~SignalBridgePlugin() = default;
+
+OpenRGBPluginInfo SignalBridgePlugin::GetPluginInfo()
+{
+    OpenRGBPluginInfo info;
+
+    info.Name = "SignalBridge Plugin";
+    info.Description = "Bridges OpenRGB signals to external integrations.";
+    info.Version = SIGNALBRIDGEPLUGIN_VERSION;
+    info.Commit = SIGNALBRIDGEPLUGIN_COMMIT;
+    info.URL = "";
+    info.Location = OPENRGB_PLUGIN_LOCATION_TOP;
+    info.Label = "SignalBridge";
+
+    return info;
+}
+
+unsigned int SignalBridgePlugin::GetPluginAPIVersion()
+{
+    return OPENRGB_PLUGIN_API_VERSION;
+}
+
+void SignalBridgePlugin::Load(ResourceManagerInterface* resource_manager_ptr)
+{
+    resource_manager = resource_manager_ptr;
+}
+
+QWidget* SignalBridgePlugin::GetWidget()
+{
+    if(widget == nullptr)
+    {
+        widget = new QWidget();
+        widget->setObjectName("SignalBridgePluginWidget");
+    }
+
+    return widget;
+}
+
+QMenu* SignalBridgePlugin::GetTrayMenu()
+{
+    return nullptr;
+}
+
+void SignalBridgePlugin::Unload()
+{
+    resource_manager = nullptr;
+    widget = nullptr;
+}
--- SignalBridgePlugin.h
+++ SignalBridgePlugin.h
@@ -1,12 +1,35 @@
 #ifndef SIGNALBRIDGEPLUGIN_H
 #define SIGNALBRIDGEPLUGIN_H
 
+#include <QMenu>
+#include <QObject>
+#include <QWidget>
+
+#include "OpenRGBPluginInterface.h"
+#include "ResourceManagerInterface.h"
 #include "SignalBridgePlugin_global.h"
 
-class SIGNALBRIDGEPLUGIN_EXPORT SignalBridgePlugin
+class SIGNALBRIDGEPLUGIN_EXPORT SignalBridgePlugin : public QObject, public OpenRGBPluginInterface
 {
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID OpenRGBPluginInterface_IID)
+    Q_INTERFACES(OpenRGBPluginInterface)
+
 public:
-    SignalBridgePlugin();
+    explicit SignalBridgePlugin(QObject* parent = nullptr);
+    ~SignalBridgePlugin() override;
+
+    OpenRGBPluginInfo GetPluginInfo() override;
+    unsigned int GetPluginAPIVersion() override;
+
+    void Load(ResourceManagerInterface* resource_manager_ptr) override;
+    QWidget* GetWidget() override;
+    QMenu* GetTrayMenu() override;
+    void Unload() override;
+
+private:
+    ResourceManagerInterface* resource_manager = nullptr;
+    QWidget* widget = nullptr;
 };
 
 #endif // SIGNALBRIDGEPLUGIN_H
